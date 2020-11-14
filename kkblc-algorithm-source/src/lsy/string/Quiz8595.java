01ee62c4da7c319c6734866ecb1fa78b1fb14339
Wonhyeong Cho <tmzkdls7989@gmail.com>
2020-11-14 20:45:58 +0900

    project init

---
+++ kkblc-algorithm-source/src/lsy/string/Quiz8595.java
@@ -0,0 +1,17 @@
+package lsy.string;
+
+/*
+ * 링크: https://www.acmicpc.net/problem/8595
+ * 제목: 히든 넘버
+ */
+
+public class Quiz8595 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    static int solution() {
+
+        return 0;
+    }
+}
