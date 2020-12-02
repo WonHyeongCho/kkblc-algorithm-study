42ba90320e9ece60c0f2067ef6e01b13a8e1975c
kimdragonhee <kim.dragon.hee@gmail.com>
2020-12-02 21:33:32 +0900

    package명 변경

---
+++ kkblc-algorithm-source/src/kyh/sortAndSearch/Quiz1026.java
@@ -0,0 +1,51 @@
+package kyh.sortAndSearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Quiz1026 {
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        //우선 한줄을 String 배열로 받아옴.
+        String strArr[]  = br.readLine().split(" ");
+        Integer aArr[] = new Integer[N];
+        //Arrays.sort에서 reverse를 쓰려면 Integer로 선언해야함
+        Integer bArr[] = new Integer[N];
+
+        //받아온 String 배열을 integer 배열에 넣음.
+        for(int i = 0; i<N;i++){
+            aArr[i] = Integer.parseInt(strArr[i]);
+        }
+        strArr  = br.readLine().split(" ");
+        //받아온 String 배열을 integer 배열에 넣음.
+        for(int i = 0; i<N;i++){
+            bArr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        //A배열 오름차순 정렬
+        Arrays.sort(aArr);
+        //B배열 내림차순 정렬.
+        Arrays.sort(bArr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // TODO Auto-generated method stub
+                return Integer.compare(o2, o1);
+            }
+        });
+        //내림 차순 방법 2. Arrays.sort(bArr, Collections.reverseOrder());
+
+
+        int answer = 0;
+        for(int i = 0; i<N;i++){
+            answer += aArr[i]*bArr[i];
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ kkblc-algorithm-source/src/kyh/stackAndQueue/Quiz2493.java
@@ -0,0 +1,82 @@
+package kyh.stackAndQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/*
+ * 링크: https://www.acmicpc.net/problem/2493
+ * 제목: 탑
+ */
+
+public class Quiz2493 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //탑의 갯수 N
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        //int 배열을 넣는 stack
+        Stack<int[]> stack = new Stack<>();
+
+        //탑의 높이를 가져오는 String 변수
+        String strArr[] = new String[N];
+        strArr = br.readLine().split(" ");
+        //다음 top의 높이
+        int tmpN = 0;
+        //0번째에는 index 저장, 1번째에는 탑의 높이를 저장.
+        int topArr[] = new int[2];
+        //첫번째는 무조건 0
+        sb.append("0 ");
+        //topArr에 0번째 push
+        topArr[0] = 0;
+        topArr[1] = Integer.parseInt(strArr[0]);
+        stack.push(topArr);
+
+        //stack pop의 탑의 높이
+        int tmpPop = 0;
+
+        //기본적으로 pop의 높이와 다음 탑의 높이를 비교
+        //pop이 높으면 pop, 다음 탑 둘다 push
+        //다음 탑이 높으면, 다음 탑만 push
+        for(int i = 1 ; i<N ; i++){
+            tmpN= Integer.parseInt( strArr[i]);
+
+            while(!stack.empty()){
+                topArr = stack.pop();
+                tmpPop = topArr[1];
+
+                if(tmpPop < tmpN){
+                    //stack이 비었으면 i번째 탑의 answer는 0
+                    if(stack.empty()) {
+                        topArr[1] = tmpN;
+                        topArr[0] = i;
+                        stack.push(topArr);
+
+                        sb.append("0 ");
+
+                        break;
+                    }
+                    //안비었으면 다음 pop과 tmpN을 다시 비교
+                }
+                else{
+                    sb.append(topArr[0]+1);
+                    sb.append(" ");
+
+                    stack.push(topArr);
+                    //메모리 새로 할당해야함.
+                    topArr= new int[2];
+
+                    topArr[0] = i;
+                    topArr[1] = tmpN;
+
+                    stack.push(topArr);
+                    break;
+                }
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
