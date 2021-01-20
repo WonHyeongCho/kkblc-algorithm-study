eecd8a341f433c49d7821fdf9c91d8808a273696
kjy <melon9363@gmail.com>
2021-01-20 22:31:59 +0900

    풀이추가

--- kkblc-algorithm-source/src/kjy/08.devide&conquer/Quiz5904.java
+++ kkblc-algorithm-source/src/kjy/08.devide&conquer/Quiz5904.java
@@ -5,14 +5,10 @@ public class Quiz5904 {
 	 * 출처: https://www.acmicpc.net/problem/5904
 	 * 풀이: 무무무
 	 * 
-	 * 1
-	 * 1 4 8
-	 * 1 4 8
-	 * 
-	 * 
 	 */
-	static String moo = "moo", curMoo = "moo";
-	static int N;
+	static String curMoo = "moo";
+	static int N, mooLength;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
@@ -25,7 +21,6 @@ public class Quiz5904 {
 				return moo.charAt(N-1);
 			}
 			curMoo = curMoo+"o";
-			moo = moo+curMoo+moo;
 			//System.out.println("cur = "+moo);
 		}
 	}
